5fa2d2938f09332450aac22f2b855e963043d981
Clebson2039 <clebsonfarias1@gmail.com>
2025-01-20 16:42:02 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Essa_tela_de_login_sai';
+}
